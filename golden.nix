8f7021e1119565f55bba54a01676286a1272a972
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-07-22 13:06:18 +0930

    Add config for golden images

---
+++ golden.nix
@@ -0,0 +1,29 @@
+{ inputs, config, pkgs, ... }:
+# Minimal config for golden images
+
+{
+  imports = [
+    ./users/kate.nix
+    ./users/ansible.nix
+  ];
+
+  time.timeZone = "Australia/Adelaide";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    ranger
+    btop
+    git
+    python3
+  ];
+
+  users = {
+    mutableUsers = false;
+    defaultUserShell = pkgs.zsh;
+  };
+
+  services.openssh.enable = true;
+
+  # If we change this things will be sad
+  system.stateVersion = "23.11";
+}
