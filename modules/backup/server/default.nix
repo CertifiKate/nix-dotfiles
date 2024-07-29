380c56648df29cbe7bb6216213a9636d51e2ebb5
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-07-29 14:32:53 +0930

    Add missing ; to backup/server

--- modules/backup/server/default.nix
+++ modules/backup/server/default.nix
@@ -12,7 +12,7 @@
       # Setup rrsync restricted authorized key
       "command='${pkgs.rsync}/rrsync -wo /var/backup/',restrict,ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIutECanUS5l5Z5FeAzgH6vxijosLGkQlcwcd7JWhez backup_write_only"
     ];
-  }
+  };
 
 
   # TODO: Add local (external drive) borg backup
