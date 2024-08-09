00df1b4c2cabdd37ddd53d75f6177aa40f40a59e
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-08-09 18:14:59 +0930

    Gnome theming

--- home-manager/roles/desktop/gnome/default.nix
+++ home-manager/roles/desktop/gnome/default.nix
@@ -22,12 +22,40 @@ in {
       ];
     };
 
+    # Visual settings
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      clock-format = "12h";
+    };
+    "org/gtk/settings/file-chooser" = {
+      clock-format = "12h";
     };
     "org/gnome/desktop/background" = {
       picture-uri = "file:///home/kate/.config/bg/${wallpaper}";
       picture-uri-dark = "file:///home/kate/.config/bg/${wallpaper}";
     };
+
+    # Keyboard shortcuts
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      move-to-workspace-1 = ["<Shift><Super>1"];
+      move-to-workspace-2 = ["<Shift><Super>2"];
+      move-to-workspace-3 = ["<Shift><Super>3"];
+      move-to-workspace-4 = ["<Shift><Super>4"];
+      close = ["<Super>q"];
+    };
+
+    # TODO: Solve this
+    # 'Custom' shortcuts
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = {
+    #   custom0 = {
+    #     name = "Launch Terminal";
+    #     binding = ["<Super>Return"];
+    #     command = "kgx";
+    #   };
+    # };
   };
 }
