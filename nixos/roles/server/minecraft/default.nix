c1ab64d3f454fcc4203488128ed2ae71de0e9b8c
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-09-04 21:31:44 +0930

    Add basic config for MC server

--- nixos/roles/server/minecraft/default.nix
+++ nixos/roles/server/minecraft/default.nix
@@ -1,7 +1,39 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  minecraft_dir = "/services/minecraft";
+in {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
   nixpkgs.overlays = [inputs.nix-minecraft.overlay];
 
-  services.minecraft-servers.enable = true;
-  # TODO: Actually get this working
+  CertifiKate.backup_service = {
+    paths = [
+      minecraft_dir
+    ];
+  };
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+
+    openFirewall = true;
+
+    dataDir = minecraft_dir;
+
+    servers.vanilla = {
+      enable = true;
+      enableReload = true;
+      package = pkgs.paperServers.paper-1_21;
+      serverProperties = {
+        motd = "Kate's NEW Minecraft server";
+        hide-online-players = true;
+        difficulty = "normal";
+        enforce-whitelist = true;
+        server-port = 25565;
+        spawn-protection = 0;
+      };
+    };
+  };
 }
