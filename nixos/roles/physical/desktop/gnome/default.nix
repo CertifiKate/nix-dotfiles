1f434f6a04b2531ecb2ce2a6cb1e3787aa4194b7
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-09-10 19:03:50 +0930

    Update flake.lock and fix gnome.exclusePackages

--- nixos/roles/physical/desktop/gnome/default.nix
+++ nixos/roles/physical/desktop/gnome/default.nix
@@ -15,21 +15,20 @@
     cheese # photo booth
     eog # image viewer
     epiphany # web browser
-    pkgs.gedit # text editor
+    gedit # text editor
     totem # video player
     yelp # help viewer
     evince # document viewer
     geary # email client
     gnome-tour
-
     gnome-calendar
     gnome-connections
-    gnome.gnome-characters
-    gnome.gnome-clocks
-    gnome.gnome-contacts
-    gnome.gnome-logs
-    gnome.gnome-maps
-    gnome.gnome-music
-    gnome.gnome-weather
+    gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-weather
   ];
 }
