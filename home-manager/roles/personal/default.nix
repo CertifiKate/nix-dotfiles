ce546829a21425a532bf4c5dab42a958773b7c7d
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2024-12-10 21:15:25 +1030

    Remove commented code

--- home-manager/roles/personal/default.nix
+++ home-manager/roles/personal/default.nix
@@ -13,32 +13,4 @@ in {
       init.defaultBranch = "main";
     };
   };
-
-  # # Add Dir Env and DevEnv
-  # programs.direnv = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  #   nix-direnv.enable = true;
-  # };
-
-  # home.packages = with pkgs; [
-  #   devenv
-  #   lazygit
-  # ];
-
-  # # Enable ZSH configuration for more specific configs (i.e adding aliases for roles)
-  # # (this /seems/ to be able to run alongside the NixOS config without issue)
-  # programs.zsh = {
-  #   enable = true;
-  #   enableBashCompletion = true;
-  #   enableCompletion = true;
-  #   oh-my-zsh = {
-  #     enable = true;
-  #     theme = "bira";
-  #     plugins = [
-  #       "direnv"
-  #       "sudo"
-  #     ];
-  #   };
-  # };
 }
