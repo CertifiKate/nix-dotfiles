033e276a3ee59de79f7aa69ff767e96c55466426
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2025-02-08 17:40:45 +1030

    Add displaylink support to laptop

--- hosts/physical/aurora/default.nix
+++ hosts/physical/aurora/default.nix
@@ -58,4 +58,8 @@
   ];
   systemd.sleep.extraConfig = "HibernateDelaySec=4h";
   networking.firewall.checkReversePath = false;
+
+  # Add DisplayLink support
+  # nix-prefetch-url --name displaylink-600.zip https://www.synaptics.com/sites/default/files/exe_files/2024-05/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.0-EXE.zip                                       255 ↵
+  services.xserver.videoDrivers = ["displaylink"];
 }
