21acc2aa0bab6a1e8c53a0a38bf5d218c90535d2
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2025-06-23 20:04:36 +0930

    Update MC server to 1.12.6

--- nixos/roles/server/minecraft/default.nix
+++ nixos/roles/server/minecraft/default.nix
@@ -25,14 +25,16 @@ in {
     servers.vanilla = {
       enable = true;
       enableReload = true;
-      package = pkgs.paperServers.paper-1_21;
+      package = pkgs.paperServers.paper-1_21_6;
       serverProperties = {
-        motd = "Kate's NEW Minecraft server";
+        motd = "Kate's NEW NEW Minecraft server";
         hide-online-players = true;
         difficulty = "normal";
         enforce-whitelist = true;
+        white-list = true;
         server-port = 25565;
         spawn-protection = 0;
+        level-seed = "1217942224227008520";
       };
     };
   };
