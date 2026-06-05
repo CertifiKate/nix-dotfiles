c6c31839c7e2a4be82ee74fec31273e1b837d7d7
CertifiKate <131977850+CertifiKate@users.noreply.github.com>
2026-06-05 21:42:21 +0930

    fix cloudflared

--- nixos/roles/server/services/cloudflared/default.nix
+++ nixos/roles/server/services/cloudflared/default.nix
@@ -8,15 +8,43 @@
 }: let
   secretsPath = toString inputs.nix-secrets;
   project_tld = "${private.project_tld}";
+  serviceName = "cloudflared-tunnel-default-tunnel";
+  credDir = "/run/credentials/${serviceName}.service";
+  setupCreds = pkgs.writeShellScript "cloudflared-setup-creds" ''
+    install -d -m 0700 -o cloudflared -g cloudflared ${credDir}
+    install -m 0400 -o cloudflared -g cloudflared \
+      ${config.sops.secrets."cloudflared_tunnel_cert".path} \
+      ${credDir}/credentials.json
+    install -m 0400 -o cloudflared -g cloudflared \
+      ${config.sops.secrets."cloudflared_tunnel_creation_cert".path} \
+      ${credDir}/cert.pem
+  '';
 in {
   config = lib.mkMerge [
     (lib.mkIf config.CertifiKate.roles.server.cloudflared.enable {
+      users.users.cloudflared = {
+        isSystemUser = true;
+        group = "cloudflared";
+      };
+      users.groups.cloudflared = {};
+
       sops.secrets."cloudflared_tunnel_cert" = {
         sopsFile = "${secretsPath}/secrets/proxy.yaml";
+        owner = "cloudflared";
       };
 
       sops.secrets."cloudflared_tunnel_creation_cert" = {
         sopsFile = "${secretsPath}/secrets/proxy.yaml";
+        owner = "cloudflared";
+      };
+
+      systemd.services.${serviceName} = {
+        serviceConfig = {
+          DynamicUser = lib.mkForce false;
+          User = "cloudflared";
+          Group = "cloudflared";
+          ExecStartPre = lib.mkBefore ["+${setupCreds}"];
+        };
       };
 
       services.cloudflared = {
